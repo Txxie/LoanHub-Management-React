3e3c45a1a31ca760b77dfaf86b060800e3e189b8
Txxie <92388470+Txxie@users.noreply.github.com>
2024-03-06 19:54:40 +0800

    使用apifox上的mock服务

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
         source: `/api/:path*`,
         // 启动mock服务，执行这个代码
         destination: `http://localhost:3001/api/:path*`,
+        // destination: `https://mock.apifox.cn/m1/2398938-0-default/api/:path*`,
         // 连接本地的nodejs服务，执行这个代码
         // destination: `https://mock.apifox.cn/m1/2398938-0-default/api/:path*`,
       },
