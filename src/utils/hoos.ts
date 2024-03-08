19f30afcbd791d66cbbaf45f0653fe709367232a
lalalion <2672806705@qq.com>
2024-03-08 22:11:00 +0800

    解决报错报错Uncaught SyntaxError: "undefined" is not valid JSON

--- src/utils/hoos.ts
+++ src/utils/hoos.ts
@@ -5,13 +5,28 @@ export const useCurrentUser = () => {
   const [user, setUser] = useState<UserType | null>(null);
   useEffect(() => {
     const obj = localStorage.getItem("user");
+    // if (obj) {
+    //   console.log(
+    //     "%c [ obj ]-9",
+    //     "font-size:13px; background:pink; color:#bf2c9f;",
+    //     obj
+    //   );
+    //   setUser(JSON.parse(obj));
+    // }
     if (obj) {
-      console.log(
-        "%c [ obj ]-9",
-        "font-size:13px; background:pink; color:#bf2c9f;",
-        obj
-      );
-      setUser(JSON.parse(obj));
+      try {
+        const parsedObj = JSON.parse(obj);
+        console.log(
+          "%c [ obj ]-9",
+          "font-size:13px; background:pink; color:#bf2c9f;",
+          parsedObj
+        );
+        setUser(parsedObj);
+      } catch (error) {
+        console.error("Error parsing JSON:", error);
+        // 处理 JSON 解析错误，例如清除 localStorage 中的无效数据
+        localStorage.removeItem("user");
+      }
     }
   }, []);
 
